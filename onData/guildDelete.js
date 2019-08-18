173d56f9493e500db69f002f02af719978387796
Briefless <Briefless@github.com>
2019-08-18 18:29:06 +0100

    Revert "Uh added stuff"
    This reverts commit 8120335624a66aeaa060f09ddc07e711994367db.

--- onData/guildDelete.js
+++
@@ -1,5 +0,0 @@
-module.exports = bot => {
-    console.log("Loaded")
-      bot.user.setActivity("Loading...")
-      bot.user.setActivity('Getting Ready For Release || ;help')
-    };
--- onData/readyEvent.js
+++
@@ -1,6 +0,0 @@
-module.exports = bot => {
-    console.log("Starting...")
-      bot.user.setActivity("Loading...")
-      bot.user.setActivity('Getting Ready For Release || ;help')
-      console.log(`Logged in as ${bot.user.username}!`); 
-  };
